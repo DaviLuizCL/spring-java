3b24c672331abc3308c8eb5d8374142bed09bba5
Davilcl <davirasenga@gmail.com>
2024-08-20 18:55:16 -0300

    Commit inicial

---
+++ src/main/java/com/davi/projeto/spring/projeto_davi_spring/startup.java
@@ -0,0 +1,13 @@
+package com.davi.projeto.spring.projeto_davi_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(startup.class, args);
+	}
+
+}
---
+++ src/test/java/com/davi/projeto/spring/projeto_davi_spring/startupTests.java
@@ -0,0 +1,13 @@
+package com.davi.projeto.spring.projeto_davi_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class startupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
